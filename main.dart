2068bb9b11ce2bcbc27b032a8b86c5b25c87715c
MATI UL REHMAN <iammatiz00@gmail.com>
2023-02-20 23:28:16 +0500

    strings

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 void main() {
   print("Hello World!");
+  String Name = "Mati ul Rehman";
 //loops
   for (int i = 0; i < 10; i++) {
     if (i == 5) {
@@ -8,4 +9,12 @@ void main() {
       print(i);
     }
   }
+  int age = 23;
+  double height = 6.4;
+  print("My name is ${Name}");
+  print("My name is ${Name} and my age is ${age} and height is ${height}.");
+  print(Name.length);
+  print(Name[0]);
+  print(Name.indexOf(" "));
+  print("Name :${Name + Name}");
 }
