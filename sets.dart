9c71649640727def7e06fc42b9cec2a22c685e00
MATI UL REHMAN <iammatiz00@gmail.com>
2023-02-27 23:38:49 +0500

    sets added

---
+++ sets.dart
@@ -0,0 +1,9 @@
+void main() {
+  //sets
+  var halogens = ['fluorine', 'chlorine', 'bromine'];
+  print(halogens);
+  print(halogens[0]);
+  halogens.add('iodine');
+  print(halogens);
+  print(halogens.isEmpty);
+}
